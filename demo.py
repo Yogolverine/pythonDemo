aef34c1e49f96958c8a0f5895b42c377c437f1d7
yogesh.kaushik <kshk.yogesh@gmail.com>
2021-05-14 20:17:15 +0530

    water

--- demo.py
+++ demo.py
@@ -1,4 +1,13 @@
 print("hello")
 ##newly added code
 print("GIHUB")
+
 print(3*4)
+
+hungry=("are you hungry")
+
+if hungry=="yes":
+    print("Eat samosa")
+    print("Take tea")
+else:
+    print("do your work")
