90cea1eadbe34b8f8bbde70bfc939521f77baace
yogesh.kaushik <kshk.yogesh@gmail.com>
2021-05-14 20:58:11 +0530

    changes to br1

--- demo.py
+++ demo.py
@@ -2,3 +2,11 @@
 print("hello")
 print("GIHUB")
 print(3*4)
+############################
+print("this is br1 branch")
+hungry=("are you hungry")
+if hungry=="yes":
+    print("Eat samosa")
+    print("Take tea")
+else:
+    print("do your work")
